54eb0ade45a60a072d51dd4a27bdb01945ab6f12
Byaidu <9097564245@qq.com>
2021-09-07 18:18:53 +0800

    Update 1.bat

--- test/1.bat
+++ test/1.bat
@@ -3,7 +3,6 @@ cls
 set a=0
 set b=0
 :loop
-rem echo #1.bat %time% 1>&2
 title time:%time%(1.bat) a:%a%(2.bat) b:%b%(3.bat)
 if %a% equ 3 (
   echo #1.bat lock 1>&2
@@ -12,6 +11,7 @@ if %a% equ 3 (
   set /a a+=1
   set thread=unlock
   echo #1.bat unlock 1>&2
+  set CAPI=API Call kernel32.dll SleepEx ;100 ;0
 )
 ping -n 2 localhost >nul
 goto loop
